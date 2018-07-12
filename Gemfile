2d7c4dc742590cc6814d1bf088d394266fe5ac7b
robotscissors <cjbazin@gmail.com>
2018-07-12 15:32:46 -0700

    add stylesheet and configured navigation

--- Gemfile
+++ Gemfile
@@ -18,9 +18,9 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
-gem 'bootstrap-sass'
 gem 'figaro'
 gem 'httparty'
+gem 'normalize-rails'
 
 group :development, :test do
   gem 'byebug'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @popular_tv = Tv.popular_tv['results'].take(6) #get top 6 for home page
+    @popular_tv = Tv.popular_tv['results'].take(12) #get top 6 for home page
   end
 end
