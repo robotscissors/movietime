8d52a5403f26c59327442941e0024a1df6fb52ac
robotscissors <cjbazin@gmail.com>
2018-07-16 20:28:27 -0700

    add session variable to retain search params

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,16 @@
 class SearchController < ApplicationController
   def index
+    #session if variable is empty then it is equal to the search_query and page number
     @search_query = params[:query]
     @page = params[:page].to_i
+    session[:current_search_term] ||= params[:query]
+    if !@search_query
+      @search_query = session[:current_search_term]
+      @page = session[:current_search_page]
+    else
+      session[:current_search_term] = @search_query
+      session[:current_search_page] = @page
+    end
     @page = 1 if @page === 0
     @search = Search.for(@search_query,@page)
     @total_pages = @search['total_pages']
